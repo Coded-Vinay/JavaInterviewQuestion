b509d766c7e79084b89475fc88414ef2c8742062
Vinay Chaudhary <mech.vinay.0201@gmail.com>
2025-08-25 17:07:23 +0530

    important hasmapinterview questions

--- JavaRefreshers/src/firstNonRepeatingChar.java
+++ JavaRefreshers/src/firstNonRepeatingChar.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.CookieHandler;
+import java.util.*;
 
 public class firstNonRepeatingChar {
 
@@ -8,18 +7,35 @@ public class firstNonRepeatingChar {
 
         String input = "mynameisvinay";
 
-        Map<Character,Integer> namemap = new HashMap<>();
+        Map<Character, Integer> namemap = new HashMap<>();
 
-        for(char ch : input.toCharArray()){
-            namemap.put(ch,namemap.getOrDefault(ch,0)+1);}
-            for(Map.Entry<Character, Integer> set : namemap.entrySet()){
-             if(set.getValue()==1){
-                 System.out.println( set.getKey() );
-             break;
-             }
+        for (char ch : input.toCharArray()) {
+            namemap.put(ch, namemap.getOrDefault(ch, 0) + 1);
+        }
+        System.out.println(namemap);
+        ArrayList<Character> ac = new ArrayList<>();
+        ArrayList<Character> bc = new ArrayList<>();
+        StringBuilder rep = new StringBuilder();
+        StringBuilder nonrep = new StringBuilder();
 
+
+
+        for (Map.Entry<Character, Integer> set : namemap.entrySet()) {
+            if (set.getValue() == 1) {
+                bc.add(set.getKey());
+                nonrep = nonrep.append(set.getKey()).append(" ");
+
+            } else {
+                ac.add(set.getKey());
+                rep = rep.append(set.getKey()).append(" ");
             }
         }
+        Collections.sort(bc);Collections.sort(ac);
+        System.out.println("repeating char >>\s" + nonrep);
+        System.out.println("non repeating char >>\s" + rep);
+
+
     }
+}
 
 
